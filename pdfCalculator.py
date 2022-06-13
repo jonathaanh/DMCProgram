7579c56d3fddcd622c7a0f54a416e29565433fb8
Jonathan Hsu <johnnhsu1031@gmail.com>
2022-06-13 16:43:46 -0700

    Added logo

--- pdfCalculator.py
+++ pdfCalculator.py
@@ -3,16 +3,16 @@
 import numpy as np
 from fpdf import FPDF
 
+#create pdf file with 8.5x11 size
 pdf = FPDF('P','mm', (215.9,279.4))
-
 pdf.add_page()
-
 pdf.set_font("Arial", size=16)
-    
+
+#add images and text 
+pdf.image("logo.jpeg")
 pdf.cell(200,10, "a = 1", ln=1, align='C')
-
 pdf.cell(200,10, "b = 2", ln=2, align='C')
-
 pdf.cell(200,10, "a+b = 3", ln=3, align='C')
 
+#output to the file name 
 pdf.output("output.pdf")
