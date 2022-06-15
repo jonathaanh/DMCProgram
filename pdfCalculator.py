0ccc6d7730b7bae4b35cc7d31d998dcc05c7ab6c
Jonathan Hsu <johnnhsu1031@gmail.com>
2022-06-15 10:54:55 -0700

    Setting up router and test html

--- pdfCalculator.py
+++ pdfCalculator.py
@@ -3,16 +3,20 @@
 import numpy as np
 from fpdf import FPDF
 
-#create pdf file with 8.5x11 size
-pdf = FPDF('P','mm', (215.9,279.4))
-pdf.add_page()
-pdf.set_font("Arial", size=16)
+class pdfCalculator(object):
 
-#add images and text 
-pdf.image("logo.jpeg")
-pdf.cell(200,10, "a = 1", ln=1, align='C')
-pdf.cell(200,10, "b = 2", ln=2, align='C')
-pdf.cell(200,10, "a+b = 3", ln=3, align='C')
+    def calculate(self,a,b):
+        #create pdf file with 8.5x11 size
+        pdf = FPDF('P','mm', (215.9,279.4))
+        pdf.add_page()
+        pdf.set_font("Arial", size=16)
+        print("I am here")
+        #add images and text 
+        pdf.image("logo.jpeg")
+        pdf.cell(200,10, "a = " + a, ln=1, align='C')
+        pdf.cell(200,10, "b = " + b, ln=1, align='C')
+        math = int(a)+int(b)
+        pdf.cell(200,10, "a+b = "+ str(math), ln=1, align='C')
 
-#output to the file name 
-pdf.output("output.pdf")
+        #output to the file name 
+        pdf.output("output.pdf")
--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import json
+from pdfCalculator import pdfCalculator
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/", methods=["POST", "GET"])
+def calculate():
+    if request.method == "POST":
+        a = request.form["a"]
+        b = request.form["b"]
+        return redirect(url_for("pdf",first=a,second=b))
+    else:
+        return render_template("calculate.html")
 
+
+@app.route("/<first>/<second>")
+def pdf(first, second): 
+    pdfCalc = pdfCalculator()
+    pdfCalc.calculate(first,second)
+    math = int(first) + int(second)
+    return f"<h1>{first} + {second} = {math}</h1>"
+    
 if __name__ == "main":
-    app.run("localhost", 6969)
+    app.run(debug=True)
