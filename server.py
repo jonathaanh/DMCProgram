c34691ebcefff280091c3b8d7270e229d7081a64
GuanyiSu <gsu@ucsd.edu>
2022-06-16 13:28:15 -0700

    basic calculator

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import json
 from pdfCalculator import pdfCalculator
@@ -10,23 +8,33 @@ app = Flask(__name__)
 def home():
     return render_template("calculate.html")
 
-@app.route("/calculate", methods=["POST", "GET"])
+@app.route("/calculate", methods=["POST"])
 def calculate():
+    print("Get a request")
     if request.method == "POST":
-        a = request.form.get("a")
-        b = request.form.get("b")
+        # a = request.form.get('a')
+        # b = request.form.get('b')
+        #print(request.data)
+        content = request.json
+        a = content['a']
+        b = content['b']
+        
+        #print(request.form.get('b'))
+        #print(a)
+        #print(b)
         math = int(a) + int(b)
+        
         #return redirect(url_for("pdf",first=a,second=b))
 
         return jsonify({"result": str(math)})
-    return "0"
+        
 
-@app.route("/pdf")
-def pdf(first, second): 
-    pdfCalc = pdfCalculator()
-    pdfCalc.calculate(first,second)
-    math = int(first) + int(second)
-    return f"<h1>{first} + {second} = {math}</h1>"
+# @app.route("/pdf")
+# def pdf(first, second): 
+#     pdfCalc = pdfCalculator()
+#     pdfCalc.calculate(first,second)
+#     math = int(first) + int(second)
+#     return f"<h1>{first} + {second} = {math}</h1>"
     
 if __name__ == "main":
     app.run(debug=True)
